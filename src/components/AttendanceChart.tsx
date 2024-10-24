a0f6cb449873b548b6fa4f26e3dc10efea708021
kapoylawas <arif.sangga@gmail.com>
2024-10-24 15:19:03 +0700

    update dasboard and chart student

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,10 @@
+const AttendanceChart = () => {
+
+    return(
+        <div className="bg-white rounded-lg p-4 h-full">
+            asdas
+        </div>
+    )
+}
+
+export default AttendanceChart
